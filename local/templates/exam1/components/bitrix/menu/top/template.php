0f72560fae6a63673233e8c583f1f8b297a2407a
Zakhar <zbybko@wejet.ru>
2024-07-17 10:24:55 +0200

    [ex1-?] Скрытая задача с установкой цвета

--- local/templates/exam1/components/bitrix/menu/top/template.php
+++ local/templates/exam1/components/bitrix/menu/top/template.php
@@ -1,29 +1,21 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+global $APPLICATION;
 
 $this->setFrameMode(true);
 
 if (empty($arResult["ALL_ITEMS"]))
   return;
-//file_put_contents(__DIR__.'/1.txt', print_r($arResult, true), FILE_APPEND);
 ?>
 <ul>
   <li class="main-page"><a href="/"><?= GetMessage('MAIN_PAGE') ?></a></li>
   <? foreach ($arResult["MENU_STRUCTURE"] as $itemID => $arColumns):
     if ($arResult["ALL_ITEMS"][$itemID]["PERMISSION"] == 'D') continue;
+    $menuTopClass = trim($APPLICATION->GetDirProperty('menu_top_class', $arResult["ALL_ITEMS"][$itemID]["LINK"]));
     ?> <!-- first level-->
     <li>
-      <a href="<?= $arResult["ALL_ITEMS"][$itemID]["LINK"] ?>">
+      <a href="<?= $arResult["ALL_ITEMS"][$itemID]["LINK"] ?>" <?=($menuTopClass ? ' class="' .$menuTopClass. '" ' : '') //Тернарник для вывода свойства если есть?>>
         <?= $arResult["ALL_ITEMS"][$itemID]["TEXT"] ?>
       </a>
       <? if (is_array($arColumns) && !empty($arColumns)): ?>
