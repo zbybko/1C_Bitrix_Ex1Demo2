d13764e34ca38c04cb8d8e55d466e7a35d4b9815
Zakhar <zbybko@wejet.ru>
2024-07-17 11:06:36 +0200

    [ex1-?] Скрытое задание, вставка включаемой области

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -144,7 +144,15 @@ IncludeTemplateLangFile(__FILE__);
       <div class="title-block"><?=GetMessage("CONTACT_INFO")?></div>
       <div class="loc-block">
         <div class="address">ул. Летняя, стр.12, офис 512</div>
-        <div class="phone"><a href="tel:84952128506">8 (495) 212-85-06</a>
+        <div class="phone">
+          <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            Array(
+              "AREA_FILE_SHOW" => "file",
+              "PATH" => "/local/templates/exam1/include/phone.php",
+            )
+          );?>
         </div>
       </div>
       <div class="main-soc-block">
